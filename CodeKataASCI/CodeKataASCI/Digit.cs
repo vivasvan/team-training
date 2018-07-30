a47e7b425a0cd6b520a3f89f6567c7c055ab3608
Viv Paranjape <vivasvanp@gmail.com>
2018-07-30 15:27:06 +0100

    asci code kata 15 min solution uploaded

---
+++ CodeKataASCI/CodeKataASCI/Digit.cs
@@ -0,0 +1,75 @@
+﻿namespace CodeKataASCI
+{
+    using System;
+
+    public class Digit
+    {
+        public Digit(int d)
+        {
+            // ._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+            // |.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+            // |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+            switch (d)
+            {
+                case 0:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "|.| ";
+                    this.ThirdLine = "|_| ";
+                    break;
+                case 1:
+                    this.FirstLine = "... ";
+                    this.SecondLine = "..| ";
+                    this.ThirdLine = "..| ";
+                    break;
+                case 2:
+                    this.FirstLine = "._.";
+                    this.SecondLine = "._| ";
+                    this.ThirdLine = "|_. ";
+                    break;
+                case 3:
+                    this.FirstLine = "... ";
+                    this.SecondLine = "|.| ";
+                    this.ThirdLine = "|_| ";
+                    break;
+                case 4:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "|_| ";
+                    this.ThirdLine = "..| ";
+                    break;
+                case 5:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "|_. ";
+                    this.ThirdLine = "._| ";
+                    break;
+                case 6:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "|_. ";
+                    this.ThirdLine = "|_| ";
+                    break;
+                case 7:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "..| ";
+                    this.ThirdLine = "..| ";
+                    break;
+                case 8:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "|_| ";
+                    this.ThirdLine = "|_| ";
+                    break;
+                case 9:
+                    this.FirstLine = "._. ";
+                    this.SecondLine = "|_| ";
+                    this.ThirdLine = "..| ";
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"source digit {d} - must be a single character representing a digit between 0 and 9");
+            }
+        }
+
+        public string FirstLine { get; set; }
+
+        public string SecondLine { get; set; }
+
+        public string ThirdLine { get; set; }
+    }
+}
---
+++ CodeKataASCI/CodeKataASCI/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CodeKataASCI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                var number = Console.ReadLine();
+
+                var numbers = (number ?? throw new InvalidOperationException()).Select(o => new Digit(int.Parse(o.ToString()))).ToList();
+
+                WriteDigits(numbers);
+
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                // ignored
+            }
+        }
+
+        private static void WriteDigits(IEnumerable<Digit> digits)
+        {
+            foreach (var digit in digits)
+            {
+                Console.Write(digit.FirstLine);
+            }
+
+            Console.Write("\r\n");
+
+            foreach (var digit in digits)
+            {
+                Console.Write(digit.SecondLine);
+            }
+
+                Console.Write("\r\n");
+
+            foreach (var digit in digits)
+            {
+                Console.Write(digit.ThirdLine);
+            }
+
+            Console.Write("\r\n");
+        }
+    }
+}
